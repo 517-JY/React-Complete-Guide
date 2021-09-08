a0b121f6a885ed12b94249af63477b37b3f310e2
Jiayin Li <jiayin.li007@outlook.com>
2021-09-08 18:57:34 -0400

    ExpensesDate/ExpensesItem/Expenses components

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,45 @@
+import ExpenseDate from "./components/ExpenseDate";
+import Expenses from "./components/Expenses";
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 295,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e2",
+      title: "New Desk",
+      amount: 96.2,
+      date: new Date(2021, 6, 15),
+    },
+    {
+      id: "e3",
+      title: "Table Lamp",
+      amount: 107.5,
+      date: new Date(2021, 3, 6),
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+// function App() {
+//   // Imperative Approach
+//   const para = document.createElement("p");
+//   para.textContent = "This is also visible";
+//   document.getElementById("root").append(para);
+//   return (
+//     <div>
+//       <h2>Let's get started!</h2>
+//     </div>
+//   );
+// }
+
+export default App;
